319be6c3100b3ccde82b8130f21974ac76c00eef
MixLab-Tech <spyfari@163.com>
2021-01-27 21:30:42 +0800

    new

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const knn = require('./src/knn');
 class Train {
     constructor() {}
 
-    async train(dataset, modelSavePath) {
+    async train(dataset) {
 
         await data.loadData(dataset);
 
@@ -16,12 +16,10 @@ class Train {
         // console.log('=', trainTexts[0])
         knn.train(trainTexts, trainLabels);
 
-
         return knn
     }
 
-    async start(dataset, modelLocalPath, bertModel, saveLocalPath) {
-        await embedding.init(modelLocalPath, bertModel);
+    async start(dataset, saveLocalPath) {
         let model = await this.train(dataset);
         let modelRes = model.export2str();
         let json = {
@@ -33,15 +31,21 @@ class Train {
     }
 }
 
+async function initEmbedding(modelLocalPath, bertModel) {
+    await embedding.init(modelLocalPath, bertModel);
+}
+
 class TextModel {
     constructor(modelLocalPath) {
         let jsonStr = fs.readFileSync(modelLocalPath, "utf-8");
         let json = JSON.parse(jsonStr);
         this.labels = json.labels;
-        knn.load(json.model)
+        let initRes = knn.load(json.model);
+        console.log('模型初始化：', initRes)
     }
     async predict(text) {
         let v = embedding.get(text);
+        // console.log(v)
         let res = await knn.predict(v);
         return this.labels[res.label];
     }
@@ -49,6 +53,7 @@ class TextModel {
 
 // run(path.join(__dirname, "model/bert_zh_L-12_H-768_A-12_2"))
 module.exports = {
+    initEmbedding: initEmbedding,
     textTrain: new Train(),
     TextModel: TextModel
 };
